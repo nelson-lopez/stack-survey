2597471c1f4bcb9d8cae75340cd0e34526de0f49
Nelson Lopez <dev.nelsonlopez@gmail.com>
2019-09-12 13:54:28 -0400

    added second graph

--- src/js/src.js
+++ src/js/src.js
@@ -7,8 +7,8 @@ let firstP = document.querySelector('.pageOne-p');
 
 // Helper functions
 const getWiki = async (lang, key) => {
-  if(lang === 'c#'){
-    lang = 'c%23'
+  if (lang === 'c#') {
+    lang = 'c%23';
   }
   let res = await axios.get(
     `https://api.stackexchange.com/2.2/tags/${lang}/wikis?site=stackoverflow&key=${key}`
@@ -17,7 +17,7 @@ const getWiki = async (lang, key) => {
   firstP.innerHTML = excerpt;
 };
 // GraphFunctions
-const getData = async language => {
+const getPie = async language => {
   let url = '';
   if (!language) {
     url = `https://api.stackexchange.com/2.2/tags?order=desc&sort=popular&site=stackoverflow&key=${key}`;
@@ -70,15 +70,64 @@ const getData = async language => {
 };
 
 // Timeseries chart
+const getTimeseries = async language => {
+  let url = '';
+  if (!language) {
+    url = `https://api.stackexchange.com/2.2/tags?order=desc&sort=popular&site=stackoverflow&key=${key}`;
+  } else if (language) {
+    url = `https://api.stackexchange.com/2.2/tags?order=desc&sort=popular&inname=${language}&site=stackoverflow&key=${key}`;
+  }
+  let res = await axios.get(url);
+  let data = res.data.items;
 
+  let parsedData = data
+    .map(arr => {
+      return [arr.name, arr.count];
+    })
+    .slice(0, 5);
+  console.log(parsedData);
 
-getData();
+  let chart2 = c3.generate({
+    bindto: '#chart2',
+    data: {
+      columns: [parsedData[0]],
+      type: 'bar',
+      onclick: function(d, i) {}
+    },
+    transition: {
+      duration: 500
+    }
+  });
+
+  setTimeout(() => {
+    chart2.load({
+      columns: [parsedData[1]]
+    });
+  }, 500);
+
+  setTimeout(() => {
+    chart2.load({
+      columns: [parsedData[2], parsedData[3]]
+    });
+  }, 1000);
+
+  setTimeout(() => {
+    chart2.load({
+      columns: [parsedData[4]]
+    });
+  }, 2000);
+
+  return chart2;
+};
+
+getPie();
+getTimeseries();
 
 // Creating API Calls
 button.addEventListener('click', () => {
   let form = document.querySelector('#form-graph1');
   let searchQ = form.value;
-  getData(searchQ);
+  getPie(searchQ);
 });
 
 // Chart generating
