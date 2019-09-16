b5e9ce21c65770336d4aa8753beb59cd901a010e
Nelson Lopez <dev.nelsonlopez@gmail.com>
2019-09-16 09:00:08 -0400

    final commit for project

--- src/js/src.js
+++ src/js/src.js
@@ -1,4 +1,6 @@
-// Global definitions
+//////////
+//// Global definitions
+/////////
 const button = document.querySelector('button');
 const title = document.createElement('h2');
 const key = 'KPD2QzLF1l6fPqcIyDQrTA((';
@@ -7,10 +9,19 @@ const qButton = document.querySelector('.questions-button');
 const qForm = document.querySelector('.questions-form');
 const list = document.querySelectorAll('li');
 const links = document.querySelectorAll('.questions-link');
+const currentTime = Math.floor(new Date().getTime() / 1000.0);
+const month = 2678400;
 
+
+
+////////
 // Helper functions
-const getWiki = async (lang, key) => {
+///////
 
+const getWiki = async (lang, key) => {
+  if (lang[lang.length - 1] === '#') {
+    lang = lang.slice(0, -1) + '%23';
+  }
   let res = await axios.get(
     `https://api.stackexchange.com/2.2/tags/${lang}/wikis?site=stackoverflow&key=${key}`
   );
@@ -20,17 +31,18 @@ const getWiki = async (lang, key) => {
 
 const getQuestions = async (key, language) => {
   let res = await axios.get(
-    `https://api.stackexchange.com/2.2/questions?fromdate=1568246400&todate=1568332800&order=desc&sort=votes&tagged=${language}&site=stackoverflow&key=${key}`
+    `https://api.stackexchange.com/2.2/questions?fromdate=${currentTime -
+      month}&todate=${currentTime}&order=desc&sort=votes&tagged=${language}&site=stackoverflow&key=${key}`
   );
   let data = res.data.items;
   let mappedQuestions = data
-    .map(arr => {
-      return arr.title;
+    .map(obj => {
+      return obj.title;
     })
     .slice(0, 5);
   let mappedLinks = data
-    .map(arr => {
-      return arr.link;
+    .map(obj => {
+      return obj.link;
     })
     .slice(0, 5);
 
@@ -40,15 +52,12 @@ const getQuestions = async (key, language) => {
     links[i].innerHTML = 'Discussion';
   }
 };
+/////////
+//// GraphFunctions
+////////
+const getPie = async () => {
+   let url = `https://api.stackexchange.com/2.2/tags?order=desc&sort=popular&site=stackoverflow&key=${key}`;
 
-// GraphFunctions
-const getPie = async language => {
-  let url = '';
-  if (!language) {
-    url = `https://api.stackexchange.com/2.2/tags?order=desc&sort=popular&site=stackoverflow&key=${key}`;
-  } else if (language) {
-    url = `https://api.stackexchange.com/2.2/tags?order=desc&sort=popular&inname=${language}&site=stackoverflow&key=${key}`;
-  }
   let res = await axios.get(url);
   let data = res.data.items;
 
@@ -67,9 +76,6 @@ const getPie = async language => {
         let name = d.name;
         getWiki(name, key);
       }
-    },
-    transition: {
-      duration: 500
     }
   });
   setTimeout(() => {
@@ -93,16 +99,71 @@ const getPie = async language => {
   return chart;
 };
 
+const getChart = async language => {
+  let url = `https://api.stackexchange.com/2.2/tags/${language}/related?site=stackoverflow&key=${key}`;
+
+  let res = await axios.get(url);
+  let data = res.data.items;
+
+  let parsedData = data
+    .map(obj => {
+      return [obj.name, obj.count];
+    })
+    .slice(0, 9);
+
+  let chart = c3.generate({
+    bindto: '.chart',
+    data: {
+      columns: [parsedData[1]],
+      type: 'bar',
+      onclick: function(d, i) {
+        let name = d.name;
+
+        getWiki(name, key);
+      }
+    }
+  });
+  setTimeout(() => {
+    chart.load({
+      columns: [parsedData[4]]
+    });
+  }, 500);
+
+  setTimeout(() => {
+    chart.load({
+      columns: [parsedData[2], parsedData[3]]
+    });
+  }, 1000);
+
+  setTimeout(() => {
+    chart.load({
+      columns: [parsedData[7]]
+    });
+  }, 2000);
+  setTimeout(() => {
+    chart.load({
+      columns: [parsedData[5], parsedData[6], parsedData[8]]
+    });
+  }, 2200);
+
+  return chart;
+};
 getPie();
 
-// Creating API Calls
+////////
+//// Event Listeners
+///////
 button.addEventListener('click', () => {
   let form = document.querySelector('.article--form');
   let searchQ = form.value;
   if (searchQ[searchQ.length - 1] === '#') {
     let sanitizedSearch = searchQ.slice(0, -1) + '%23';
-    getPie(sanitizedSearch);
-  } else getPie(searchQ);
+    getChart(sanitizedSearch);
+    getWiki(sanitizedSearch, key);
+  } else {
+    getChart(searchQ);
+    getWiki(searchQ, key);
+  }
 });
 
 qButton.addEventListener('click', e => {
